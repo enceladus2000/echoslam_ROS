258a53bde32d28315a3becb90317fb557203ef7f
adbidwai <adbidwai@gmail.com>
2020-08-01 00:19:38 +0530

    changed package name, modifed launch files

--- scripts/bot.py
+++ scripts/bot.py
@@ -36,7 +36,9 @@ pub = rospy.Publisher(robot.topic_name, Bot, queue_size=1)
 
 def main():
 	try:
-		robot.id, robot.teamsize = cl_args()
+		#robot.id, robot.teamsize = cl_args()
+		robot.teamsize = rospy.get_param("size")
+		robot.id = rospy.get_param("id")
 	except ValueError as error:
 		print(error)
 		print('Exiting...')
@@ -55,27 +57,27 @@ def main():
 
 	
 
-def cl_args():
-	if len(sys.argv) <=1:
-		raise ValueError('Please enter id and teamsize.')
+# def cl_args():
+# 	if len(sys.argv) <=1:
+# 		raise ValueError('Please enter id and teamsize.')
 		
-	try:
-		optlist, _ = getopt.getopt(sys.argv[1:], "i:n:", ['id=','teamsize=']) 
-	except getopt.GetoptError:
-		raise ValueError('Invalid arguments!')
+# 	try:
+# 		optlist, _ = getopt.getopt(sys.argv[1:], "i:n:", ['id=','teamsize=']) 
+# 	except getopt.GetoptError:
+# 		raise ValueError('Invalid arguments!')
 
-	bot_id = None
-	teamsize = None
-	for opt, arg in optlist:
-		if opt in ('-i', '--id'):
-			bot_id = int(arg)
-		elif opt in ('-n', '--teamsize'):
-			teamsize = int(arg)
+# 	bot_id = None
+# 	teamsize = None
+# 	for opt, arg in optlist:
+# 		if opt in ('-i', '--id'):
+# 			bot_id = int(arg)
+# 		elif opt in ('-n', '--teamsize'):
+# 			teamsize = int(arg)
 
-	if bot_id is None or teamsize is None:
-		raise ValueError('Please enter both id and teamsize!')
+# 	if bot_id is None or teamsize is None:
+# 		raise ValueError('Please enter both id and teamsize!')
 
-	return bot_id, teamsize
+# 	return bot_id, teamsize
 
 if __name__ == '__main__':
 	main()
