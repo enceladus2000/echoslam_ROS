985326a1c8bdf9e942441e57c2fabf6496246f4e
enceladus2000 <tpb2k99@gmail.com>
2020-08-05 18:05:40 +0530

    cleaned up bot.py

--- scripts/bot.py
+++ scripts/bot.py
@@ -3,21 +3,16 @@
 from echoslam.msg import Bot
 import sys
 import rospkg
+import rospy
+import getopt
+from random import random
+
+# import robot class from src folder
 rospack = rospkg.RosPack()
 path = rospack.get_path('echoslam_ROS')
 sys.path.append(path)
 import src
 from src.robot import Robot
-import rospy
-import getopt
-import json
-from random import random
-
-"""TODO
-1. Calc relative position
-2. Add multiple transducers to robot class
-3. trilateration alg (w/o error)
-"""
 
 def callback(msg):
 	if msg.id.data != robot.id:
@@ -26,7 +21,6 @@ def callback(msg):
 		print('bot_id:', msg.id.data)
 		print('bot_pos: {x: .2f},{y: .2f}'.format(x=msg.x.data, y=msg.y.data))
 
-
 	# check if bot that just transmitted is the one before
 	if msg.id.data % robot.teamsize == robot.id - 1:
 		print('My turn to transmit! Waiting for {:.2f}s...'.format(transmit_delay))
@@ -34,24 +28,24 @@ def callback(msg):
 		print('Transmitting...')
 		pub.publish(robot.msg)
 
-transmit_delay = 2.0	# time delay between transmissions, in seconds
+transmit_delay = 1.0	# time delay between transmissions, in seconds
 robot = Robot()
 sub = rospy.Subscriber(robot.topic_name, Bot, callback)
-pub = rospy.Publisher(robot.topic_name, Bot, queue_size=1)
+pub = rospy.Publisher(robot.topic_name, Bot, queue_size=3)
 
 def main():
+	# procure teamsize and id from param server
 	try:
-		#robot.id, robot.teamsize = cl_args()
 		robot.teamsize = rospy.get_param("/size")
 		robot.id = rospy.get_param("id")
-	except ValueError as error:
-		print(error)
+	except KeyError as error:
+		print('The parameter {} was not found'.format(error))
 		print('Exiting...')
 		sys.exit(2)
 
 	robot.initRandomPos((0, 0), (10, 10))
 	robot.createMsg()
-	rospy.init_node("dummy_node_name")
+	rospy.init_node("dummy_node_name")		# required??
 	print('Starting {bot}...'.format(bot=robot.getBotName()))
 
 	# break the ice
@@ -62,27 +56,27 @@ def main():
 
 	
 
-# def cl_args():
-# 	if len(sys.argv) <=1:
-# 		raise ValueError('Please enter id and teamsize.')
+def cl_args():
+	if len(sys.argv) <=1:
+		raise ValueError('Please enter id and teamsize.')
 		
-# 	try:
-# 		optlist, _ = getopt.getopt(sys.argv[1:], "i:n:", ['id=','teamsize=']) 
-# 	except getopt.GetoptError:
-# 		raise ValueError('Invalid arguments!')
+	try:
+		optlist, _ = getopt.getopt(sys.argv[1:], "i:n:", ['id=','teamsize=']) 
+	except getopt.GetoptError:
+		raise ValueError('Invalid arguments!')
 
-# 	bot_id = None
-# 	teamsize = None
-# 	for opt, arg in optlist:
-# 		if opt in ('-i', '--id'):
-# 			bot_id = int(arg)
-# 		elif opt in ('-n', '--teamsize'):
-# 			teamsize = int(arg)
+	bot_id = None
+	teamsize = None
+	for opt, arg in optlist:
+		if opt in ('-i', '--id'):
+			bot_id = int(arg)
+		elif opt in ('-n', '--teamsize'):
+			teamsize = int(arg)
 
-# 	if bot_id is None or teamsize is None:
-# 		raise ValueError('Please enter both id and teamsize!')
+	if bot_id is None or teamsize is None:
+		raise ValueError('Please enter both id and teamsize!')
 
-# 	return bot_id, teamsize
+	return bot_id, teamsize
 
 if __name__ == '__main__':
 	main()
