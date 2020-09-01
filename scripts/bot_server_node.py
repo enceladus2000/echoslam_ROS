bb5b881b620801db9da306be03e2ee0b369c47b6
adbidwai <adbidwai@gmail.com>
2020-09-01 14:22:53 +0530

    formatted code

--- scripts/bot_server_node.py
+++ scripts/bot_server_node.py
@@ -1,49 +1,58 @@
-#! /usr/bin/env python 
+#! /usr/bin/env python
 
-import rospy  
 import random
-from math import sqrt
-from echoslam_ROS.srv import BotService, BotServiceResponse
-import rospkg
 import sys
-rospack = rospkg.RosPack()
-path = rospack.get_path('echoslam_ROS')
-sys.path.append(path)
+from math import sqrt
+
+import rospkg
+import rospy
+from echoslam_ROS.srv import BotService, BotServiceResponse
+
 from src.robot import Robot
 
-bots = rospy.get_param('/size')
-radius =rospy.get_param('/radius')
+rospack = rospkg.RosPack()
+path = rospack.get_path("echoslam_ROS")
+sys.path.append(path)
+
+bots = rospy.get_param("/size")
+radius = rospy.get_param("/radius")
 pose_list = []
 
+
 def pose_generator():
-	global pose_list
-	dist_list = []
-	i = 0
-	distance = lambda point : sqrt((pose_list[i][0]-point[0])**2 + (pose_list[i][1]-point[1])**2)
-	point = [bots*random.random(), bots*random.random()]
-	if(len(pose_list)==0):
-		pose_list.append(point)
-		return point
-	for i in range(len(pose_list)):
-		dist_list.append(distance(point))
-	while(min(dist_list)<=2*radius):
-		point = [bots*random.random(), bots*random.random()]
-		for i in range(len(pose_list)):
-			dist_list[i] = distance(point)
-	pose_list.append(point)
-	return point
+    global pose_list
+    dist_list = []
+    i = 0
+    distance = lambda point: sqrt(
+        (pose_list[i][0] - point[0]) ** 2 + (pose_list[i][1] - point[1]) ** 2
+    )
+    point = [bots * random.random(), bots * random.random()]
+    if len(pose_list) == 0:
+        pose_list.append(point)
+        return point
+    for i in range(len(pose_list)):
+        dist_list.append(distance(point))
+    while min(dist_list) <= 2 * radius:
+        point = [bots * random.random(), bots * random.random()]
+        for i in range(len(pose_list)):
+            dist_list[i] = distance(point)
+    pose_list.append(point)
+    return point
+
 
 response_obj = BotServiceResponse()
 
+
 def bot_server(request_obj):
-	response_obj.bot.x.data = pose_generator()[0]
-	response_obj.bot.y.data = pose_generator()[1]
-	response_obj.bot.id.data = request_obj.id.data
-	###ADD RANDOM NUMBER ALGO HERE
-	return response_obj
+    response_obj.bot.x.data = pose_generator()[0]
+    response_obj.bot.y.data = pose_generator()[1]
+    response_obj.bot.id.data = request_obj.id.data
+    ###ADD RANDOM NUMBER ALGO HERE
+    return response_obj
+
 
 rospy.init_node("bot_server_node")
-bot_service = rospy.Service('/bot_service', BotService, bot_server)
+bot_service = rospy.Service("/bot_service", BotService, bot_server)
 print("#######################")
 print("BOT-SERVER INITIALIZED")
 print("#######################")
